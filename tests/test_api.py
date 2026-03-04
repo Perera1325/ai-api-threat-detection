b4b196d962bd5fe7bf474775cf8aad69ab4e25f7
Vinod Ravindu Perera <bamunuge2002@gmail.com>
2026-03-04 11:13:59 +0530

    Update API test with authentication headers

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,30 +1,16 @@
 import requests
-import time
 
 url = "http://127.0.0.1:8000/api"
 
-normal = {
+headers = {
+    "x-api-key":"123456"
+}
+
+data = {
     "ip":"192.168.1.10",
     "requests_per_minute":10
 }
 
-attack = {
-    "ip":"192.168.1.50",
-    "requests_per_minute":500
-}
+r = requests.post(url,json=data,headers=headers)
 
-print("Normal request")
-print(requests.post(url,json=normal).json())
-
-print("AI attack test")
-print(requests.post(url,json=attack).json())
-
-print("Rate limit attack")
-
-for i in range(25):
-
-    r = requests.post(url,json=normal)
-
-    print(i,r.json())
-
-    time.sleep(0.1)
+print(r.json())
